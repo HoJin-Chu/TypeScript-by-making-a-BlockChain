0d3af4fab47d84b9ed76fb2e43881b375af1d94c
hojin-chu <cnghwls12@naver.com>
2019-08-16 02:08:42 +0900

    create Block structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
-const name = "chuhojin",
-  age = 24,
-  gender = "male"
-
-const say = (name:string, age:number, gender:string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`
+// Block Structure
+class Block {
+  public index:number 
+  public hash:string
+  public previousHash:string
+  public data:string
+  public timestamp:number
+  constructor(index:number, hash:string, previousHash:string, data:string, timestamp:number) {
+    this.index = index
+    this.hash = hash
+    this.previousHash = previousHash
+    this.data = data
+    this.timestamp = timestamp
+  }
 }
 
-console.log(say("chuhojin", 25, "maile"))
+const genesisBlock:Block = new Block(0, "12345412312", "", "Hello", 123456)
+
+let blockchain: [Block] = [genesisBlock]
+
+console.log(blockchain)
+
+// class Human {
+//   public name: string 
+//   public age: number
+//   public gender: string
+//   constructor(name: string, age:number, gender:string) {
+//     this.name = name
+//     this.age = age
+//     this.gender = gender
+//   }
+// }
+
+// const chu = new Human("CHU", 25, "male")
+
+// const say = (person: Human): string => {
+//   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+// }
+
+// console.log(say(chu))
 
 
 export {}
