22a8c93db113594473bc5f994995317c466cf733
hojin-chu <cnghwls12@naver.com>
2019-08-16 02:23:50 +0900

    block

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js"
+
 // Block Structure
 class Block {
   public index:number 
@@ -5,7 +7,21 @@ class Block {
   public previousHash:string
   public data:string
   public timestamp:number
-  constructor(index:number, hash:string, previousHash:string, data:string, timestamp:number) {
+
+  static calculateBlockHash = (
+    index:number,
+    previousHash:string, 
+    timestamp:number, 
+    data:string
+  ) :string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+
+  constructor(
+    index:number, 
+    hash:string, 
+    previousHash:string, 
+    data:string, 
+    timestamp:number
+  ) {
     this.index = index
     this.hash = hash
     this.previousHash = previousHash
@@ -16,9 +32,13 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "12345412312", "", "Hello", 123456)
 
-let blockchain: [Block] = [genesisBlock]
+let blockchain: Block[] = [genesisBlock]
 
-console.log(blockchain)
+const getBlockchain = ():Block[] => blockchain
+
+const getLatestBlock = ():Block => blockchain[blockchain.length - 1]
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime() / 1000)
 
 // class Human {
 //   public name: string 
